aaac7e8568d182a4b6199d87c154175a52c79db9
leonguevara <leon.guevara@outlook.com>
2016-04-06 12:13:03 -0500

    Initial Commit

---
+++ myMapApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myMapApp
+//
+//  Created by León Felipe Guevara Chávez on 2016-04-06.
+//  Copyright © 2016 León Felipe Guevara Chávez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
