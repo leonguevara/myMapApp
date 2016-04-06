3a00d89e014357a1be670a8d849d09ed6ab3b748
leonguevara <leon.guevara@outlook.com>
2016-04-06 13:00:15 -0500

    Estado inicial de la aplicación.  Se colocó el mapa y tres botones para cambiar entre las diferentes vistas del mapa.

--- myMapApp/ViewController.swift
+++ myMapApp/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapa: MKMapView!
+    @IBOutlet var vistaNormal: [UIButton]!
+    @IBOutlet weak var vistaSatélite: UIButton!
+    @IBOutlet weak var vistaHíbrida: UIButton!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        mapa.zoomEnabled = true
+        mapa.showsUserLocation = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func cambiarVista(sender: UIButton) {
+        if (sender.tag == 1) {
+            // Seleccionó la vista normal
+            mapa.mapType = .Standard
+        } else if (sender.tag == 2) {
+            // Seleccionó la vista satelital
+            mapa.mapType = .Satellite
+        } else {
+            // Seleccionó la vista híbrida
+            mapa.mapType = .Hybrid
+        }
+    }
 
 }
 
